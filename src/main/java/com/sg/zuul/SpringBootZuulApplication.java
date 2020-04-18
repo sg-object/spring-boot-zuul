e876168a2558bb44a2807da488d10302a8571f7f
sg.lee <sg.lee@oneplat.co>
2020-04-18 11:17:35 +0900

    init

---
+++ src/main/java/com/sg/zuul/SpringBootZuulApplication.java
@@ -0,0 +1,13 @@
+package com.sg.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZuulApplication.class, args);
+	}
+
+}
